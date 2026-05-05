71e0dfb7a61b53f05e5d803e1d8b454ee8b48e2c
Roman <romazaniosdev@gmail.com>
2026-05-05 10:17:01 +0300

    Initial Commit

---
+++ 156PesokortrexNendel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  156PesokortrexNendel
+//
+//  Created by Roman on 5/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
